82f4982006912970bbe6f999673afa9d3339658f
Ashwariya Gopal <aishwariyagopal@gmail.com>
2025-02-25 13:43:56 +0500

    first commit

--- Project1-todo_list/script.js
+++ Project1-todo_list/script.js
@@ -59,6 +59,9 @@ const updateStats = () => {
     const progressBar = document.getElementById('progress')
     progressBar.style.width = `${progress}%`;
     document.getElementById('numbers').innerText = `${completeTasks} / ${totalTasks}`
+    if(tasks.length && completeTasks === totalTasks) {
+        blastConfetti()
+    }
 
 
 }
@@ -98,3 +101,44 @@ addTask();
 })
 
 
+const blastConfetti = () => {
+    const count = 200,
+  defaults = {
+    origin: { y: 0.7 },
+  };
+
+function fire(particleRatio, opts) {
+  confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio),
+    })
+  );
+}
+
+fire(0.25, {
+  spread: 26,
+  startVelocity: 55,
+});
+
+fire(0.2, {
+  spread: 60,
+});
+
+fire(0.35, {
+  spread: 100,
+  decay: 0.91,
+  scalar: 0.8,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 25,
+  decay: 0.92,
+  scalar: 1.2,
+});
+
+fire(0.1, {
+  spread: 120,
+  startVelocity: 45,
+});
+}
